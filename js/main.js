34f6e8a8312d770cb5d39fdcc19dcb285c5af4cc
Camila Sáez <cfsaezbravo@gmail.com>
2017-07-13 19:23:09 -0400

    Listo

--- js/main.js
+++ js/main.js
@@ -7,3 +7,38 @@ $(".button-collapse").sideNav();
 $(document).ready(function(){
    $('.collapsible').collapsible();
 });
+
+/*Traer el JSON*/
+
+$(document).ready(function(){
+  $('.btn-calcular').click(function() {
+
+  	var infoInput = $('.num_tarjeta').val();
+      
+    if($('.num_tarjeta').val() === ''){
+        alert("Debes completar el campo");
+    }else{
+       $.ajax({
+           	url: 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip='+infoInput,
+           	type: 'GET',
+           	dataType: 'json',
+           	data: {limit: '1200'},
+           })
+           .done(function(val) {
+           	console.log(urlTarjeta);
+           	console.log("success");
+           	console.log(val);
+           	$('.project').append('<p>Saldo de la tarjeta:'+val.saldoTarjeta+'</p>')
+           })
+           .fail(function() {
+           	console.log("error");
+           })
+           .always(function() {
+           	console.log("complete");
+           });
+               
+        }
+    });
+});
+
+
